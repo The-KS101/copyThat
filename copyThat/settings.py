efc93efca58b10a6a4a683265fa69976c4c8cd8d
Takuro Sesan <sesantakuro29@gmail.com>
2021-02-15 00:17:06 +0100

    Initial Setup of app

--- copyThat/settings.py
+++ copyThat/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2a8k2v(c8yy)-0f#k20nfjtw4yn%jc8@^**79qdpl&gq9r08#n'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pasteIt'
 ]
 
 MIDDLEWARE = [

77e25aa673d5c0c6c1c677da643f33cb7204bbe9
Olamilekan Rasaq Aremu <aremulekan@gmail.com>
2021-02-15 04:23:18 +0300

    Update forms.py

--- pasteIt/forms.py
+++ pasteIt/forms.py
@@ -3,4 +3,3 @@ from django import forms
 class ContentPasted(forms.Form):
     content = forms.CharField(label=" ")
     delTime = forms.DateTimeField(label="Expiry", input_formats=['%d/%m/%Y %H:%M'])
-

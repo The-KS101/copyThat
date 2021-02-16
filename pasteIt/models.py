bb0dcc26e86ec5133f9d0538f60195dd4aee3539
Takuro Sesan <sesantakuro29@gmail.com>
2021-02-16 20:20:19 +0100

    Update

--- pasteIt/models.py
+++ pasteIt/models.py
@@ -2,8 +2,10 @@ from django.db import models
 
 # Create your models here.
 
-class urlTable (models.Model):
+class urlTable(models.Model):
     url = models.CharField(max_length=20, blank=False, default = None)
     text = models.TextField(blank=False, default = None)
     deletion_time = models.DateTimeField()
 
+    def __str__(self):
+        return self.url

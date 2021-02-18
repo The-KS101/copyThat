86a7a297319675fb57745de97af0caa12ff5bc95
Takuro Sesan <sesantakuro29@gmail.com>
2021-02-18 03:03:56 +0100

    Back End Comp

--- pasteIt/views.py
+++ pasteIt/views.py
@@ -4,74 +4,42 @@ from .forms import ContentPasted
 import datetime
 from datetime import timezone
 
+#renders the index view which showcases a form and runs some data processing before entry creation in db
 def index(request):
-    form = ContentPasted()
-    if request.method == "POST":
-        form = ContentPasted(request.POST)
-        if form.is_valid():
-            url = form.cleaned_data['url']
-            text = form.cleaned_data['text']
-            deletion_time = datetime.datetime.now() + datetime.timedelta(minutes=int(form.cleaned_data['delTime']))
-            urlTable.objects.create(url=url, text=text, deletion_time=deletion_time, visited=False)
-            return render(request, 'created.html')
-
-
-    context = {
-        'form': form,
-    }
-    return render(request, 'index.html', context)
+    return render(request, 'index.html')
 
 def urlReceive(request, urlName):
     form = ContentPasted()
     try:
         #working on this, might look complicated now
-        
         data = urlTable.objects.get(url=urlName)
-        if datetime.datetime.now().replace(tzinfo=timezone.utc) > data.deletion_time.replace(tzinfo=timezone.utc):
+        if datetime.datetime.now().replace(tzinfo=timezone.utc) > data.deletion_time and data.visited==True:
             data.delete()
-            give_form(request)
-            
+            return give_populated_form_with_url(request, urlName)
 
         else:
-            form = ContentPasted(initial={"urll":urlName})
+            form = ContentPasted(initial={'url': data.url, 'text': data.text})
+            data.visited = True
+            data.save()
+            return give_form(request, form)
 
 
-        
     except:
-        give_populated_form_with_url(request, urlName)
+        return give_populated_form_with_url(request, urlName)
 
-
-        # context = {
-        #     'form': form,
-        # }
-        # return render(request, 'pasted.html', context)
-
-    
-    # form = ContentPasted(initial={'url': urlName, 'text':data.text })
-    context = {
-        'form': form,
-    }
-    return render(request, 'pasted.html', context)
 
 #This is the function to returns a functional form
 
-def give_form(request):
-    form = ContentPasted()
-    form.url = urlName
-    if request.method == "POST":
-        form = ContentPasted(request.POST)
-        if form.is_valid():
-            url = form.cleaned_data['url']
-            text = form.cleaned_data['text']
-            deletion_time = datetime.datetime.now() + datetime.timedelta(minutes=int(form.cleaned_data['delTime']))
-            urlTable.objects.create(url=url, text=text, deletion_time=deletion_time)
-            return render(request, 'created.html')
+def give_form(request, form):
+    context = {
+        'form': form,
+    }
+    return render(request, 'displayPasted.html', context)
 
 #This gives the functional form with the url popualated
 
 def give_populated_form_with_url(request, urlName):
     form = ContentPasted(initial={'url': urlName})
-    form.url = urlName
     if request.method == "POST":
         form = ContentPasted(request.POST)
         if form.is_valid():
@@ -80,4 +48,7 @@ def give_populated_form_with_url(request, urlName):
             deletion_time = datetime.datetime.now() + datetime.timedelta(minutes=int(form.cleaned_data['delTime']))
             urlTable.objects.create(url=url, text=text, deletion_time=deletion_time)
             return render(request, 'created.html')
-
+    context = {
+        'form': form,
+    }
+    return render(request, 'pasted.html', context)
